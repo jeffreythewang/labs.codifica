4c6c46a9cd339225219856eacabb5799ea0af7de
Jeff Wang <jeffreywang93@gmail.com>
2015-09-05 15:25:21 -0400

    compiles jsx lol

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,6 +1,8 @@
 package controllers
 
+import java.io.FileReader
 import javax.inject.{Singleton, Inject}
+import javax.script.ScriptEngineManager
 
 import play.api.Play
 import play.api.Play.current
@@ -8,14 +10,22 @@ import play.api.mvc._
 
 import org.slf4j.{LoggerFactory, Logger}
 
-@Singleton
-class Application @Inject() extends Controller {
-
-  private final val logger: Logger = LoggerFactory.getLogger(classOf[Application])
+object Application extends Controller {
 
   def index = Action {
-    logger.info("Serving index page...")
 
-    Ok(views.html.index())
+    val engine = new ScriptEngineManager(null).getEngineByName("nashorn")
+
+    if (engine == null) {
+      BadRequest("Nashorn script engine not found. Are you using JDK 8?")
+    } else {
+      engine.eval("var global = this;")
+      engine.eval("var console = {error: print, log: print, warn: print};")
+
+      engine.eval(new FileReader("target/web/web-modules/main/webjars/lib/react/react-with-addons.js"))
+      engine.eval(new FileReader("target/web/public/main/js/App.js"))
+      Ok(views.html.index())
+    }
+
   }
 }
--- build.sbt
+++ build.sbt
@@ -16,5 +16,5 @@ libraryDependencies ++= Seq(
   "javax.inject"       % "javax.inject"        % "1",
   "org.reactivemongo" %% "play2-reactivemongo" % "0.10.5.0.akka23",
   "org.webjars"       %% "webjars-play"        % "2.3.0-3",
-  "org.webjars"       %% "react"               % "0.13.1"
+  "org.webjars"        % "react"               % "0.13.1"
 )
