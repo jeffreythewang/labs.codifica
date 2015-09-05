469ab94a634293d448a0322f70482ea9ed53db7e
Jeff Wang <jeffreywang93@gmail.com>
2015-09-05 19:48:46 -0400

    update schema

--- app/models/Recipe.scala
+++ app/models/Recipe.scala
@@ -1,13 +1,23 @@
 package models
 
-case class Recipe(method: String,
-                  grindSetting: Int,
-                  beansWeight: Int,
-                  waterWeight: Int,
-                  tastingNotes: String)
+case class RecipeTypes(types: Seq[String])
+
+case class RecipeList(method: String, list: Seq[Recipe])
+
+case class Recipe(
+  author: String,
+  method: String,
+  relative_img_path: String,
+  grind_setting: Int,
+  beans_weight: Int,
+  water_weight: Int,
+  tasting_notes: String,
+  directions: String
+)
 
 object JsonFormats {
   import play.api.libs.json.Json
 
   implicit val recipeFormat = Json.format[Recipe]
+  implicit val recipeListFormat = Json.format[RecipeList]
 }
